857b30fcc26660d37feae9290d3828e9cf15f532
murungarigrace-arch <murungarigrace@gmail.com>
2025-09-27 10:26:45 +0300

    Add files via upload

---
+++ gracemurungari.js
@@ -0,0 +1,33 @@
+//I'm declaring my function here
+function isEven(num){
+if (num % 2 === 0){
+   return true;
+} else {
+   return false;
+}
+}
+
+//Test cases
+console.log(isEven(4)); //Expected output: true
+console.log(isEven(7)); //Expected output: false
+console.log(isEven(-2)); //Expected output: true//I'm declaring my function here
+function isEven(num){
+if (num % 2 === 0){
+   return true;
+} else {
+   return false;
+}
+}
+
+//Test cases
+console.log(isEven(4)); //Expected output: true
+console.log(isEven(7)); //Expected output: false
+console.log(isEven(-2)); //Expected output: true
+
+
+@murungarigrace-arch ➜ /workspaces/even (main) $ nano func.js
+@murungarigrace-arch ➜ /workspaces/even (main) $ node func.js
+true
+false
+true
+@murungarigrace-arch ➜ /workspaces/even (main) $ 
